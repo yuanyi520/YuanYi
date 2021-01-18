9b28857a3619d3581cc4d5b736c132be0b8d2749
Administrator <1185491050@qq.com>
2021-01-18 21:43:20 +0800

    引入mybtisa 测试xml和注解

--- src/main/java/com/yuanyi/artemis/ArtemisApplication.java
+++ src/main/java/com/yuanyi/artemis/ArtemisApplication.java
@@ -1,5 +1,6 @@
 package com.yuanyi.artemis;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/yuanyi/artemis/controller/UserController.java
+++ src/main/java/com/yuanyi/artemis/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.yuanyi.artemis.controller;
 
+import com.yuanyi.artemis.mapper.dao.StudentTest01;
 import com.yuanyi.artemis.service.UserService;
 import com.yuanyi.artemis.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,10 +25,41 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private StudentTest01 studentTest01;
+
     @GetMapping(value = "/userList", produces = "application/json;charset=UTF-8")
     public String userList(){
         try {
             return responseUtil.success("请求成功",userService.getUserList());
+        }catch (Exception e){
+            e.printStackTrace();
+            return responseUtil.error(e.getMessage(),null);
+        }
+    }
+
+    /**
+     * mybaits xml方法
+     * @return
+     */
+    @GetMapping(value = "/userList2", produces = "application/json;charset=UTF-8")
+    public String userList2(){
+        try {
+            return responseUtil.success("请求成功",studentTest01.selectAll());
+        }catch (Exception e){
+            e.printStackTrace();
+            return responseUtil.error(e.getMessage(),null);
+        }
+    }
+
+    /**
+     * mybaits xml方法
+     * @return
+     */
+    @GetMapping(value = "/findUserById/{id}", produces = "application/json;charset=UTF-8")
+    public String findUserById(@PathVariable("id") Integer id){
+        try {
+            return responseUtil.success("请求成功",studentTest01.findUserById(id));
         }catch (Exception e){
             e.printStackTrace();
             return responseUtil.error(e.getMessage(),null);
