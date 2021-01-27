6edfb20e658a6dc83bc2aff7696483ca9986ceab
Administrator <1185491050@qq.com>
2021-01-27 15:38:39 +0800

    初始化前端代码

--- src/main/java/com/yuanyi/artemis/controller/JumpPageController.java
+++ src/main/java/com/yuanyi/artemis/controller/JumpPageController.java
@@ -1,13 +1,13 @@
 package com.yuanyi.artemis.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JumpPageController {
 
-    @GetMapping("/test")
-    public String getTest(){
-        return "Test";
+    @RequestMapping("/")
+    public String index(){
+        return "index";
     }
 }
