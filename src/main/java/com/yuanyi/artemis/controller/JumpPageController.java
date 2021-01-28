dc2c6bde9d90024146852b47e6e4697d20a9084c
Administrator <1185491050@qq.com>
2021-01-28 17:33:18 +0800

    添加修改MarkDown文件插件-editormd 添加博客详情页面

--- src/main/java/com/yuanyi/artemis/controller/JumpPageController.java
+++ src/main/java/com/yuanyi/artemis/controller/JumpPageController.java
@@ -3,11 +3,36 @@ package com.yuanyi.artemis.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 页面跳转控制器
+ */
 @Controller
 public class JumpPageController {
 
+    /**
+     * 主页跳转
+     * @return
+     */
     @RequestMapping("/")
     public String index(){
         return "index";
+    }
+
+    /**
+     * 新增博客
+     * @return
+     */
+    @RequestMapping("/addBlog")
+    public String addBlog(){
+        return "addBlog";
+    }
+
+    /**
+     * 博客详情
+     * @return
+     */
+    @RequestMapping("/detailBlog")
+    public String detailBlog(){
+        return "detailBlog";
     }
 }
